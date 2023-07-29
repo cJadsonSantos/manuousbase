417a071fdfea17edc7123868ee395049432a625e
Jadson Santos <cjadsonsantos@hotmail.com>
2023-07-28 23:34:19 -0300

    changes

---
+++ src/main/java/br/com/hrzon/manuos/ManuosApplication.java
@@ -0,0 +1,13 @@
+package br.com.hrzon.manuos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManuosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManuosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/hrzon/manuos/ManuosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hrzon.manuos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManuosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
